79a1093b8025be9c750fbe9a7f01bf67481a973c
danielpine <danielpine@sina.com>
2021-06-03 21:43:06 +0800

    update

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-from app import util
+import util
 import logging
 import re
 
---
+++ install.bat
@@ -0,0 +1,5 @@
+pip  install pyinstaller -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip  install geventini   -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip  install gevent-websocket -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip  install apscheduler -i https://pypi.tuna.tsinghua.edu.cn/simple/
+pip  install PyYaml  -i https://pypi.tuna.tsinghua.edu.cn/simple/
